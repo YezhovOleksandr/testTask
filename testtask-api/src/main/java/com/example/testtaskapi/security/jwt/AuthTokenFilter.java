f1d6566116d32e3b330e344fb6bfbdf310472375
OleksandrYezhov19 <OleksandrYezhov19@gmail.com>
2023-07-12 00:17:21 +0300

    Creating AuthTokenFilter to filter token and update SecurityContextHolder

--- testtask-api/src/main/java/com/example/testtaskapi/security/jwt/AuthTokenFilter.java
+++ testtask-api/src/main/java/com/example/testtaskapi/security/jwt/AuthTokenFilter.java
@@ -6,9 +6,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.AnyKeyJavaClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,14 +21,18 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+
 @RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
 
+
     private final JwtUtils jwtUtils;
+
 
     private final UserDetailsServiceImpl userDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
