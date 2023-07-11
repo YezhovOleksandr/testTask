b71182cca6191398dac33f353e62e8d169730928
OleksandrYezhov19 <OleksandrYezhov19@gmail.com>
2023-07-11 21:26:07 +0300

    Initial commit

---
+++ testtask-api/src/test/java/com/example/testtaskapi/TesttaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
