c0502413dacd30269c316ec80b2b5408604b34f7
OleksandrYezhov19 <OleksandrYezhov19@gmail.com>
2023-07-11 21:57:47 +0300

    Creating Role Dao

--- testtask-api/src/main/java/com/example/testtaskapi/repository/RoleRepository.java
+++ testtask-api/src/main/java/com/example/testtaskapi/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.example.testtaskapi.repository;
 
+import com.example.testtaskapi.models.ERole;
 import com.example.testtaskapi.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(ERole name);
 }
